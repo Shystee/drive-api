926398974c750c90c2a559469caa4dca6ca7f809
Arminas Makulis <arminasmakulis@gmail.com>
2025-06-18 19:04:01 +0300

    feat: file upload

---
+++ src/Drive.Api/Persistence/Migrations/20250618154513_Initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Drive.Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "files",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    content_type = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    original_file_name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    s3_key = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    size_in_bytes = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_files", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_files_s3_key",
+                table: "files",
+                column: "s3_key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "files");
+        }
+    }
+}
--- src/Drive.Api/Program.cs
+++ src/Drive.Api/Program.cs
@@ -1,12 +1,61 @@
+using Amazon.S3;
+using Amazon.SecretsManager;
+using Drive.Api;
+using Drive.Api.Core.SecretManager;
+using JasperFx.Resources;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
-using System.Reflection;
+using Wolverine;
+using Wolverine.AmazonSqs;
+using Wolverine.EntityFrameworkCore;
+using Wolverine.Http;
+using Wolverine.Http.FluentValidation;
+using Wolverine.Postgresql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Configuration
+builder.Configuration.AddAmazonSecretsManager("eu-central-1", "rds!db-7632abcf-8a9e-4b8a-a497-3f23e20c3884");
+builder.Configuration.AddAmazonSecretsManager("eu-central-1", "drive-api");
+var host = builder.Configuration["host"];
+var username = builder.Configuration["username"];
+var password = builder.Configuration["password"];
+var connectionString = $"Host={host};Database=drive;Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
+builder.Services.AddWolverineHttp();
+
+// Services
+builder.Services.AddAWSService<IAmazonSecretsManager>();
+builder.Services.AddAWSService<IAmazonS3>();
+builder.Services.AddResourceSetupOnStartup();
+builder.Services.AddDbContextWithWolverineIntegration<ApplicationDbContext>(x =>
+{
+	x.UseNpgsql(connectionString);
+});
+
+// Host
+builder.Host.UseWolverine(opts =>
+{
+	opts.UseEntityFrameworkCoreTransactions();
+	opts.Policies.AutoApplyTransactions();
+	opts.PersistMessagesWithPostgresql(connectionString, "wolverine");
+	opts.UseAmazonSqsTransport().AutoProvision().AutoPurgeOnStartup();
+
+	opts.PublishMessage<StartUpload>().ToSqsQueue("start-upload");
+	opts.ListenToSqsQueue("start-upload");
+	opts.ListenToSqsQueue("upload-completed")
+		.ReceiveRawJsonMessage(typeof(S3UploadCompleted), o =>
+		{
+			o.Converters.Add(new S3EventToUploadCompletedConverter());
+		});
+
+	opts.PublishMessage<UploadTimeout>().ToSqsQueue("upload-timeout");
+	opts.ListenToSqsQueue("upload-timeout");
+	opts.PublishMessage<CreateFileCommand>().ToSqsQueue("create-file");
+	opts.ListenToSqsQueue("create-file");
+});
 
 var app = builder.Build();
 
@@ -19,18 +68,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var version = Assembly.GetExecutingAssembly()
-	              .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-	              ?.InformationalVersion ??
-              "1.0.0";
-
 app.MapHealthChecks("/healthz");
-app.MapGet("/version", () => new
-	{
-		version,
-		timestamp = DateTime.UtcNow
-	})
-	.WithName("GetVersion")
-	.WithOpenApi();
+app.MapWolverineEndpoints(opts =>
+{
+	opts.UseFluentValidationProblemDetailMiddleware();
+});
 
 app.Run();
